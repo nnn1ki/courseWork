47437b5cf2caa371930cf173ad00a73a75dd81b0
Your Name <you@example.com>
2022-12-21 01:56:22 +0800

    start make gravitation

--- Emitter.cs
+++ Emitter.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,69 @@ namespace courseWork
 {
     class Emitter
     {
+        List<Particle> particles = new List<Particle>();
+        public int MousePositionX;
+        public int MousePositionY;
+
+        public float GravitationX = 0;
+        public float GravitationY = 1; 
+
+        public void UpdateState()
+        {
+            foreach (var particle in particles)
+            {
+                particle.Life -= 1;
+                if (particle.Life < 0)
+                {
+                    // тоже не трогаем
+                    particle.Life = 20 + Particle.rand.Next(100);
+                    particle.X = MousePositionX;
+                    particle.Y = MousePositionY;
+
+                    var direction = (double)Particle.rand.Next(360);
+                    var speed = 1 + Particle.rand.Next(10);
+
+                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
+                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
+
+                    particle.Radius = 2 + Particle.rand.Next(10);
+                }
+                else
+                {
+                    particle.SpeedX += GravitationX;
+                    particle.SpeedY += GravitationY;
+
+                    // это не трогаем
+                    particle.X += particle.SpeedX;
+                    particle.Y += particle.SpeedY;
+                }
+
+                for (var i = 0; i < 10; ++i)
+                {
+                    if (particles.Count < 500)
+                    {
+                        var part = new ParticleColorful();
+                        
+                        part.FromColor = Color.Black;
+                        part.ToColor = Color.FromArgb(0, Color.Magenta);
+                        part.X = MousePositionX;
+                        part.Y = MousePositionY;
+                        particles.Add(part);
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        public virtual void Render(Graphics g)
+        {
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
+        }
     }
 }
--- Particle.cs
+++ Particle.cs
@@ -16,46 +16,31 @@ namespace courseWork
         public float SpeedY; // скорость перемещения по оси Y
 
         public float Life; 
-
         public static Random rand = new Random();
 
         public Particle()
         {
-            // генерируем произвольное направление и скорость
             var direction = (double)rand.Next(360);
             var speed = 1 + rand.Next(10);
 
-            // рассчитываем вектор скорости
             SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
             SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
 
-            // а это не трогаем
             Radius = 2 + rand.Next(10);
             Life = 20 + rand.Next(100);
         }
 
         public virtual void Draw(Graphics g)
         {
-            // рассчитываем коэффициент прозрачности по шкале от 0 до 1.0
-            float k = Math.Min(1f, Math.Abs(Life) / 100);
-            // рассчитываем значение альфа канала в шкале от 0 до 255
-            // по аналогии с RGB, он используется для задания прозрачности
-
+            float k = Math.Min(1f, Life / 100);
             int alpha = (int)(k * 255);
             
-            // создаем цвет из уже существующего, но привязываем к нему еще и значение альфа канала
-            var color = Color.FromArgb(alpha, Color.White);
+            var color = Color.FromArgb(alpha, Color.Black);
             var b = new SolidBrush(color);
 
-            // остальное все так же
             g.FillEllipse(b, X - Radius, Y - Radius, Radius * 2, Radius * 2);
-
             b.Dispose();
         }
-
-
-
-
 
     }
 
@@ -80,7 +65,7 @@ namespace courseWork
         // ну и отрисовку перепишем
         public override void Draw(Graphics g)
         {
-            float k = Math.Min(1f, Math.Abs(Life) / 100);
+            float k = Math.Min(1f, Life / 100);
 
             var color = MixColor(ToColor, FromColor, k);
             var b = new SolidBrush(color);
